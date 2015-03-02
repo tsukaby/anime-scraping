1beee8c35142293bc34c18bcd72f3c15aed09b0b
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-03-02 15:26:33 +0900

    引数で年代を指定する形式に変更

--- src/main/scala/com/tsukaby/anime_scraping/controller/ScrapingController.scala
+++ src/main/scala/com/tsukaby/anime_scraping/controller/ScrapingController.scala
@@ -4,7 +4,6 @@ import java.io.File
 
 import com.github.tototoshi.csv.CSVWriter
 import com.tsukaby.anime_scraping.service.ScrapingService
-import com.tsukaby.anime_scraping.service.Season.{Autumn, Summer, Spring, Winter}
 
 /**
  * Sample
@@ -14,12 +13,14 @@ class ScrapingController extends BaseController {
   val scrapingService: ScrapingService = ScrapingService
 
   override protected def runMain(args: Array[String]): Unit = {
+    require(args.length == 1)
+
     // リンクを取得
-    val year = 2015
+    val year = args(0).toInt
     val animesPageUrl = s"http://ja.wikipedia.org/wiki/Category:${year}%E5%B9%B4%E3%81%AE%E3%83%86%E3%83%AC%E3%83%93%E3%82%A2%E3%83%8B%E3%83%A1"
     val links = scrapingService.getWikiLinks(animesPageUrl)
 
-    val f = new File("out.csv")
+    val f = new File(s"animes_$year.csv")
     val writer = CSVWriter.open(f)
     writer.writeRow("title" :: "url" :: "thumbnailDelay" :: "season" :: "year" :: Nil)
 
@@ -28,7 +29,7 @@ class ScrapingController extends BaseController {
       val anime = scrapingService.getAnime(x._2)
       println(anime)
 
-      val season = anime.season.map(_.toString.toLowerCase) getOrElse ""
+      val season = anime.season.map(_.toString.replaceAll("$", "").toLowerCase) getOrElse ""
 
       writer.writeRow(anime.name :: anime.officialSiteUrl.getOrElse("") :: "0" :: season :: anime.startDateTime.map(_.getYear).getOrElse("") :: Nil)
     }
